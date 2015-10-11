f1f62252c45d20c750785a5591bff903e1c71bb4
Ovan Pulu <ovan89@gmail.com>
2015-10-12 06:18:07 +0700

    index

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
-<?php
+<?php 
 session_start();
 
-
+/* define codekir version */
 define ('CODEKIR', 'Version 1.0');
 
-define ('COREPATH', 'engine/');
+define ('CODEKIR_TEMPLATE', 'infovol');
+/* define app path */
+define ('CODEKIR_ROOTPATH', '/home/ovancop/Data/htdocs/infovolunteer/website/');
 
-require_once (COREPATH.'system.php');
+/* define engine path */
+define ('CODEKIR_PACKAGE', '/home/ovancop/Data/htdocs/codekir/new-codekir/loader/');
 
+define ('debug', true);
+/* 
+	define loader path, do not edit this path
+*/
+define ('CODEKIR_APPPATH', CODEKIR_ROOTPATH . 'applications/default/');
+define ('CODEKIR_COREPATH', CODEKIR_PACKAGE . 'engine/');
+define ('CODEKIR_LIBS', CODEKIR_PACKAGE . 'plugin/');
+define ('CODEKIR_LOGS', CODEKIR_PACKAGE . 'logs/');
+define ('CODEKIR_CACHE', CODEKIR_PACKAGE . 'cache/');
+define ('CODEKIR_TMP', CODEKIR_PACKAGE . 'tmp/');
+
+require_once (CODEKIR_APPPATH.'config/main.php');
 
 ?>
